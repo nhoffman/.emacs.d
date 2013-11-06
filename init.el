dc96e1fa09f908f6bdbeadb14e1e10f722f7f8d5
Noah Hoffman <noah.hoffman@gmail.com>
2013-11-06 11:36:34 -0800

    add toggle-frame-split

--- init.el
+++ init.el
@@ -263,6 +263,20 @@
     (switch-to-buffer other-frame-buffer)))
 (global-set-key (kbd "C-x 5") 'switch-buffers-between-frames)
 
+(defun toggle-frame-split ()
+  "If the frame is split vertically, split it horizontally or vice versa.
+Assumes that the frame is only split into two."
+  (interactive)
+  (unless (= (length (window-list)) 2) (error "Can only toggle a frame split in two"))
+  (let ((split-vertically-p (window-combined-p)))
+    (delete-window) ; closes current window
+    (if split-vertically-p
+        (split-window-horizontally)
+      (split-window-vertically)) ; gives us a split with the other window twice
+    (switch-to-buffer nil))) ; restore the original window in this part of the frame
+
+(global-set-key (kbd "C-x 6") 'toggle-frame-split)
+
 (setq-default ispell-program-name "aspell")
 (setq ispell-dictionary "en")
 
