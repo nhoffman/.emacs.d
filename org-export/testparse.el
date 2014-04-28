24af81f19cf9bc694e7d120ae45edc0121592b37
Noah Hoffman <noah.hoffman@gmail.com>
2014-04-27 23:06:51 -0700

    play with alsists

--- org-export/testparse.el
+++ org-export/testparse.el
@@ -1,7 +1,17 @@
 (require 'argparse (concat (file-name-directory load-file-name) "argparse.el"))
 
 ;; get command line options
-(setq infile (get-option args "infile"))
-(setq outfile (get-option args "outfile"))
+;; (setq infile (get-option args "infile"))
+;; (setq outfile (get-option args "outfile"))
 
-(print args)
+;; (print args)
+
+(setq options-alist
+      '(("infile" "foo")
+	("--outfile" "bar")
+	("--buh"))
+      )
+
+(print options-alist)
+(print (cdr (assoc "--outfile" options-alist)))
+(print (assoc "--buh" options-alist))
