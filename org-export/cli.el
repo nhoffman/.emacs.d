2999ad8c4c96e8322612b155c36df4e1572cfc23
Noah Hoffman <noah.hoffman@gmail.com>
2014-05-02 22:20:54 -0700

    add package management to cli

--- org-export/cli.el
+++ org-export/cli.el
@@ -1,5 +1,6 @@
 (setq lexical-binding t)
 (provide 'cli)
+(require 'package)
 
 (defun cli-do-nothing () t)
 
@@ -7,7 +8,7 @@
   "Return t if str is not nil and starts with '--'"
   (and str (string-equal (substring str 0 2) "--")))
 
-(defun cli-get-name (str)
+(defun cli-opt-name (str)
   "Return the option name (strips leading '--')"
   (substring str 2 nil))
 
@@ -55,13 +56,13 @@ value of `cli-do-nothing'.
     ;; set defaults
     (mapc #'(lambda (optdef)
 	      (if (eq (length optdef) 3)
-		  (puthash (cli-get-name (car optdef)) (nth 2 optdef) args))
+		  (puthash (cli-opt-name (car optdef)) (nth 2 optdef) args))
 	      ) options-alist)
 
     ;; set options from command line arguments
     (while clargs
       (setq opt (car clargs))
-      (setq optname (if (cli-option-p opt) (cli-get-name opt)))
+      (setq optname (if (cli-option-p opt) (cli-opt-name opt)))
       (if optname
 	  (progn
 	    (setq optiondef (assoc opt options-alist))
@@ -78,7 +79,7 @@ value of `cli-do-nothing'.
 
     ;; check for required arguments
     (mapc #'(lambda (optdef)
-	      (setq hashval (gethash (cli-get-name (car optdef)) args))
+	      (setq hashval (gethash (cli-opt-name (car optdef)) args))
 	      (if (and (eq (length optdef) 2) (not hashval))
 		  (error (format "Error: a value for the option '%s' is required."
 				 (car optdef))))
@@ -86,3 +87,40 @@ value of `cli-do-nothing'.
 
     args
     ))
+
+;; package management
+
+(defun cli-package-installed-not-builtin-p (package &optional min-version)
+  "Return true if PACKAGE, of MIN-VERSION or newer, is installed,
+ignoring built in packages.  MIN-VERSION should be a version
+list."
+  (let ((pkg-desc (assq package package-alist)))
+    (if pkg-desc
+        (version-list-<= min-version
+                         (package-desc-vers (cdr pkg-desc))))))
+
+(defun cli-install-packages (package-list)
+  "Install each package named in PACKAGE-LIST using elpa if not
+already installed."
+  (mapc #'(lambda (pkg)
+	    (unless (cli-package-installed-not-builtin-p pkg)
+	      (package-menu-refresh)
+	      (package-install pkg))
+	    ) package-list))
+
+(defvar cli-default-package-archives
+  '(("gnu" . "http://elpa.gnu.org/packages/")
+    ("marmalade" . "http://marmalade-repo.org/packages/")
+    ))
+
+(defun cli-package-setup (emacs-directory package-list &optional upgrade archives)
+  (setq user-emacs-directory emacs-directory)
+  (package-initialize)
+  (setq package-archives (or archives cli-default-package-archives))
+  (package-list-packages-no-fetch)
+  (if upgrade
+      (progn
+	(package-menu-mark-upgrades)
+	(package-menu-execute)))
+  (cli-install-packages package-list))
+
--- org-export/testparse.el
+++ org-export/testparse.el
@@ -2,25 +2,17 @@
 
 (byte-compile-file (concat (file-name-directory load-file-name) "cli.el"))
 
-;; get command line options
-;; (setq infile (get-option args "infile"))
-;; (setq outfile (get-option args "outfile"))
-
-;; (print args)
-
-;; (--option help default)
 (setq options-alist
       '(("--infile" "input file")
 	("--outfile" "output file")
-	("--foo" "an argument named foo" "bar")
-	("--baz" "make baz true" nil)
+	("--package-dir" "Directory containing elpa packages" "~/.org-export")
+	("--package-upgrade" "Perform package upgrade" nil)
 	))
 
-;; (print (describe-function 'cli-parse-args))
-
 (setq args (cli-parse-args options-alist))
-(print args)
-;; (print (cli-parse-args options-alist '("--infile" "FOO" "--outfile" "BAR")))
+(defun clarg (name) (gethash name args))
 
+(cli-package-setup
+ (clarg "package-dir") '(ess htmlize org) (clarg "package-upgrade"))
 
 
