fde2d9d7ad29c7b2d62200e57f5f138da54be759
Noah Hoffman <noah.hoffman@gmail.com>
2014-08-30 10:59:32 -0700

    load pw faster

--- init-fast.el
+++ init-fast.el
@@ -1,24 +1,15 @@
 (unless (= emacs-major-version 24)
   (error "Emacs version 24 is required"))
 
+(menu-bar-mode -1)   ;; hide menu bar
+(tool-bar-mode -1)   ;; hide tool bar
+(scroll-bar-mode -1) ;; hide scroll bar
+
 (require 'package)
 
 ;; relies on packages already present in ./elpa
+(setq package-load-list '(org))
 (package-initialize)
-
-;; ;; Original Emacs Lisp Package Archive
-;; (add-to-list 'package-archives
-;; 	     '("elpa" . "http://tromey.com/elpa/") t)
-;; ;; User-contributed repository
-;; ;; Marmalade is for packages that cannot be uploaded to the official ELPA repository.
-;; (add-to-list 'package-archives
-;; 	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
-;; (add-to-list 'package-archives
-;; 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
-;; (add-to-list 'package-archives
-;; 	     '("org" . "http://orgmode.org/elpa/") t)
-;; (add-to-list 'package-archives
-;; 	     '("elpy" . "http://jorgenschaefer.github.io/packages/") t)
 
 (require 'epa-file)
 (setenv "GPG_AGENT_INFO" nil) ;; suppress graphical passphrase prompt
