e2ec0f2a4f5a61b8fbb04e5471f78c712540307f
Noah Hoffman <noah.hoffman@gmail.com>
2015-01-29 10:25:32 -0800

    remap expand-region commands to something that works ovet the terminal

--- init.el
+++ init.el
@@ -274,8 +274,8 @@
 
 (define-key global-map (kbd "M-'") 'ace-jump-mode)
 
-(global-set-key (kbd "C-=") 'er/expand-region)
-(global-set-key (kbd "C-M-=") 'er/contract-region)
+(global-set-key (kbd "M-=") 'er/expand-region)
+(global-set-key (kbd "M-+") 'er/contract-region)
 
 (defvar my-key-map-prefix "C-c l")
 
