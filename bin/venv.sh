708dfac55c6ce5c3bd5db14efdcd165b24f5106d
Noah Hoffman <noah.hoffman@gmail.com>
2017-11-18 21:23:33 -0800

    make venv for py2 and py3

--- bin/venv.sh
+++ bin/venv.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# set up virtualenvs for python2 and python3
+
 set -e
 
-VENV=emacs-env
-virtualenv $VENV
-source $VENV/bin/activate
-pip install -U pip
-pip install -U -r requirements.txt
+VENV2=~/.emacs.d/python2-env
+virtualenv $VENV2
+$VENV2/bin/pip install -U pip
+$VENV2/bin/pip install -U -r requirements.txt
 
+VENV3=~/.emacs.d/python3-env
+python3 -m venv $VENV3
+$VENV3/bin/pip install -U pip
+$VENV3/bin/pip install -U -r requirements.txt
