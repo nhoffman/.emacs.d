5b1a09bb4caea5e7a2c4589c1e1b7d8f0c562850
Noah Hoffman <noah.hoffman@gmail.com>
2019-01-31 09:25:27 -0800

    configure org-mode in init-fast.el

--- init-fast.el
+++ init-fast.el
@@ -22,3 +22,35 @@
 
 (custom-set-variables
   '(safe-local-variable-values (quote ((toggle-read-only . t)))))
+
+;; org-mode
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (message "Loading org-mode hooks")
+             ;; (font-lock-mode)
+             ;; (setq org-confirm-babel-evaluate nil)
+             (setq org-src-fontify-natively t)
+             (setq org-edit-src-content-indentation 0)
+             (define-key org-mode-map (kbd "M-<right>") 'forward-word)
+             (define-key org-mode-map (kbd "M-<left>") 'backward-word)
+             ;; provides key mapping for the above; replaces default
+             ;; key bindings for org-promote/demote-subtree
+             (define-key org-mode-map (kbd "M-S-<right>") 'org-do-demote)
+             (define-key org-mode-map (kbd "M-S-<left>") 'org-do-promote)
+             (visual-line-mode)
+             ;; org-babel
+
+             ;; enable a subset of languages for evaluation in code blocks
+             (setq my/org-babel-load-languages
+                   '((python . t)
+                     (sql . t)
+                     (sqlite . t)
+                     (emacs-lisp . t)))
+
+             ;; use "shell" for org-mode versions 9 and above
+             (add-to-list 'my/org-babel-load-languages
+                          (if (>= (string-to-number (substring (org-version) 0 1)) 9)
+                              '(shell . t) '(sh . t)))
+
+             (org-babel-do-load-languages
+              'org-babel-load-languages my/org-babel-load-languages)))
